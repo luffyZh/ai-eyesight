75c46bd03db96a9773eb923755c78cdc20df79c1
zhoudeyou <zhoudeyou@cnaeit.com>
2025-03-18 09:53:29 +0800

    feat: 增加部署

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH,
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react(), tailwindcss()],
 })
